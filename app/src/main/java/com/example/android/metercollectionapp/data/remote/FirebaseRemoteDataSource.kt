0af90da69c7b6f4cd46014f103ca9d9cd50f8bcf
ivankoshmanskii <koshmansky@yandex.ru>
2023-05-10 19:58:36 -0700

    authentification algorithm debugged ok.

--- app/src/main/java/com/example/android/metercollectionapp/data/remote/FirebaseRemoteDataSource.kt
+++ app/src/main/java/com/example/android/metercollectionapp/data/remote/FirebaseRemoteDataSource.kt
@@ -1,35 +1,41 @@
 package com.example.android.metercollectionapp.data.remote
 
 import android.util.Log
+import com.example.android.metercollectionapp.SyncStatus
 import com.example.android.metercollectionapp.data.remote.entities.RemoteUser
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
 private const val EMAIL = "@gmail.com"
+private const val COLLECTION_USERINFO = "userinfo"
+private const val COLLECTION_USERINFO_EMAIL_FIELD = "email"
+private const val COLLECTION_USERINFO_FULLNAME_FIELD = "fullname"
 
 class FirebaseRemoteDataSource : RemoteDataSource {
+
+    private val db: FirebaseFirestore by lazy {
+        Firebase.firestore
+    }
 
     private var firebaseAuth: FirebaseAuth? = null
     private var firebaseUser: FirebaseUser? = null
 
-    // добавить к логину постфикс "@gmail.com"
     private fun getUserEmailByLogin(userLogin: String) = userLogin + EMAIL
 
-    // попытка аутентификации
     override suspend fun authUser(remoteUser: RemoteUser): RemoteUser? {
-        var fullUserName = ""  // возвращается при успешной аутентификации, полное имя пользователя находится на сервере
+        val email = getUserEmailByLogin(remoteUser.login)
+        val pass = remoteUser.passEncrypted  // пароли в Room храняться в зашифрованном виде
         var result: RemoteUser? = null
 
         if (firebaseAuth == null) firebaseAuth = Firebase.auth
         firebaseAuth?.let { auth ->
-            val email = getUserEmailByLogin(remoteUser.login)
-            val pass = remoteUser.passEncrypted  // пароли в Room храняться в зашифрованном виде
 
-            // возвращается либо currentUser либо null если аутентификация не прошла
             firebaseUser = suspendCoroutine { continuation ->
 
                 auth.signInWithEmailAndPassword(email, pass)
@@ -45,6 +51,38 @@ class FirebaseRemoteDataSource : RemoteDataSource {
                     }
             }
 
+            if (firebaseUser != null) {
+                // аутентификация прошла успешно
+                // получить структуру с подробной информацией о пользователях
+                val fullName: String? = suspendCoroutine { continuation ->
+
+                    db.collection(COLLECTION_USERINFO)
+                        .whereEqualTo(COLLECTION_USERINFO_EMAIL_FIELD, email)
+                        .get()
+                        .addOnSuccessListener { documents ->
+                            var fullName: String? = null
+                            for (document in documents) {
+                                if (document.data.containsKey(COLLECTION_USERINFO_FULLNAME_FIELD)) {
+                                    fullName = document.data[COLLECTION_USERINFO_FULLNAME_FIELD] as? String
+                                }
+                                break  // только первый элемент
+                            }
+                            continuation.resume(fullName)
+                        }
+                        .addOnFailureListener { exception ->
+                            exception.printStackTrace()
+                            continuation.resume(null)
+                        }
+                }
+
+                if (fullName != null) {
+                    // подробная информация о пользователе прочитана успешно
+                    result = remoteUser.copy(
+                        status = SyncStatus.SUCCESS,
+                        name = fullName
+                    )
+                }
+            }
         }
 
         return result
